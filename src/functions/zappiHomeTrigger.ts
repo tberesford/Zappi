af5fb219d036cb2424272eef802ad750e36b541b
Tom Beresford <tberesfords@outook.com>
2025-01-02 12:54:12 +0000

    Updated to not impact overrides outside of schedule

--- src/functions/zappiHomeTrigger.ts
+++ src/functions/zappiHomeTrigger.ts
@@ -1,6 +1,16 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
 import { MyZappi } from "../Zappi";
 
+function GetModeChange(hour: number, minute: number): number{
+    if((hour == 23 && minute >= 30) || (hour == 0 && minute == 0)){
+        return 3;
+    } else if((hour == 5 && minute >= 30) || (hour == 6 && minute == 0)){
+        return 4;
+    } else {
+        return 0;
+    }
+}
+
 export async function zappiHomeTrigger(myTimer: Timer, context: InvocationContext): Promise<void> {
     const datetime = new Date();
     const hour = datetime.getHours();
@@ -11,23 +21,16 @@ export async function zappiHomeTrigger(myTimer: Timer, context: InvocationContex
         const myZappiStatus = await myZappiObj.getZappiStatus();
         context.info(`Zappi Mode is currently: ${myZappiStatus.zmo}`);
 
-        if((hour == 23 && minute >= 30) || (hour == 0 && minute < 30)){ // Additional checks outside of period in case of missed step
-            myZappiObj.ChargeMode = 3;        
-        } else if((hour == 5 && minute >= 30) || (hour == 6 && minute < 30)){
-            myZappiObj.ChargeMode = 4;
+        const chargeMode = GetModeChange(hour, minute);
+        // Only change the Zappi mode if within schedule
+        if(chargeMode==0){
+            context.info("No change in charge mode - out of schedule.");
+        } else {
+            myZappiObj.ChargeMode = chargeMode;
+            myZappiObj.changeZappiMode();
+            context.info(`Charge mode changed to mode ${myZappiObj.ChargeMode}`);
         }
-    
-        try{
-            if(myZappiStatus.zmo != myZappiObj.ChargeMode){
-                myZappiObj.changeZappiMode();
-                setTimeout(async () => {
-                    const newStatus = await myZappiObj.getZappiStatus();
-                    console.info(`Zappi Mode is now: ${newStatus.zmo}`);
-                }, 10000);
-            }
-        } catch (error) {
-            context.error(error);
-        }
+
         context.info('Timer function processed request.');
     } catch (error) {
         context.error(error);
